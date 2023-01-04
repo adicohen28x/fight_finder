8aff89abedaa4cf4cd8020ddb23e7b9b1b71a2e1
Adi Cohen <93610739+adicohen28x@users.noreply.github.com>
2023-01-04 13:27:55 +0200

    kiwi gets

--- main.py
+++ main.py
@@ -6,4 +6,24 @@ import requests
 from dotenv import load_dotenv
 load_dotenv()
 
+sheet_name = os.getenv("GOOGLE_SHEET_NAME")
+sheet_url = os.getenv("SHEET_URL")
+cities_get_url = os.getenv("CITY_CODE_URL")
+kiwi_api = os.getenv("KIWI_KEY")
+# getting cities names from sheet:
+get_cities = requests.get(url=sheet_url)
+
+cities_list = [item['city'] for item in get_cities.json()['prices']]
+
+city_headers={
+    "apikey":kiwi_api,
+    "accept":"application/json"
+}
+
+cities_codes=[]
+for i in cities_list:
+    kiwi_get= requests.get(url=f"{cities_get_url}?term={i}", headers=city_headers)
+    cities_codes.append(kiwi_get.json()['locations'][0].get('code'))
+
+print(cities_codes)
 
