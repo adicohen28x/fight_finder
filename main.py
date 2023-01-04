8efe59398db67ce6c7d3bbc00cde39bfb23d9464
Adi Cohen <93610739+adicohen28x@users.noreply.github.com>
2023-01-04 12:26:13 +0200

    env

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+
+import os
+import datetime
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+
